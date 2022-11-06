ab7501092db79d4d54bfade5c38e83a09772e7ea
Esteban Londoño Reyes <mila-l93@hotmail.com>
2022-11-06 22:34:51 +0100

    Tema1

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //Tipos de datos
+
+        //1.1 Enteros
+        byte num = 127;
+        System.out.println(num);
+        short num1 = 32502;
+        System.out.println(num1);
+        int num2 = 4203912;
+        System.out.println(num2);
+        long num3 = 556712398;
+        System.out.println(num3);
+
+        //1.2 Decimales
+        float num4 = 1.234f;
+        System.out.println(num4);
+        double num5 = 1.23567;
+        System.out.println(num5);
+
+        //1.3 Booleanos
+        boolean cielo = true;
+        System.out.println(cielo);
+        boolean marSeco = false;
+        System.out.println(marSeco);
+
+        //1.4 Strings
+        char caracter = 'a';
+        System.out.println(caracter);
+        String fin = "Final";
+        System.out.println(fin);
+
+    }
+}
